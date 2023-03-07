dd606b51014eb16de818dd095e73522160911892
Suleyman Baris Eser <suleymanbariseser@gmail.com>
2023-03-07 22:13:47 +0300

    redirect user if not a member

--- app/routes/channels/$channelId.tsx
+++ app/routes/channels/$channelId.tsx
@@ -1,12 +1,34 @@
-import type { LoaderArgs } from '@remix-run/node';
+import type { LoaderArgs} from '@remix-run/node';
+import { redirect } from '@remix-run/node';
 import { useLoaderData } from '@remix-run/react';
-import ChatMessage from '~/components/chat/chat-message';
 import { db } from '~/utils/db.server';
 import ChatHeader from '~/components/chat/chat-header';
 import ChatInput from '~/components/chat/chat-input';
 import ChatBody from '~/components/chat/chat-body';
+import { requireUserId } from '~/utils/session.server';
 
 export const loader = async ({ request, params }: LoaderArgs) => {
+  const userId = await requireUserId(request, '/login');
+  const channelId = params.channelId;
+
+  if(!channelId) {
+    throw redirect('/channels');
+  }
+  
+  // if user does not have rights to view this channel, redirect to home page
+  const userChannel = await db.userChannels.findUnique({
+    where: {
+      userId_channelId: {
+        userId: +userId,
+        channelId: channelId,
+      }
+    }
+  })
+
+  if(!userChannel) {
+    throw redirect('/channels');
+  }
+    
   const channel = await db.channel.findUnique({
     where: {
       id: params.channelId,
